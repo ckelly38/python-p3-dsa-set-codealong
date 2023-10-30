403755a4841dd74933f097ed2a98c186faa917a1
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-10-30 17:42:45 -0600

    this creates a basic set class in python

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,16 @@
 class MySet:
+    def __init__(self, mlist):
+        self.dictionary = {};
+        for item in mlist: self.dictionary.update({item: True});
+    
+    def add(self, val):
+        self.dictionary[val] = True;
 
-    pass
+    def has(self, val):
+        return val in self.dictionary;
+
+    def delete(self, val):
+        self.dictionary.pop(val);
+
+    def size(self):
+        return len(self.dictionary);
